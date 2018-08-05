43f99a111b0259a350b2b93d6932341bfc487b11
Vinay S Shenoy <vinaysshenoy@gmail.com>
2018-08-05 19:45:34 +0530

    Add single digit gesture for moving objects and panning the view

--- app/src/main/kotlin/com/vinaysshenoy/jescher/sample/CustomView.kt
+++ app/src/main/kotlin/com/vinaysshenoy/jescher/sample/CustomView.kt
@@ -3,6 +3,7 @@ package com.vinaysshenoy.jescher.sample
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.PointF
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
@@ -26,7 +27,10 @@ class CustomView @JvmOverloads constructor(
 	attributeSet: AttributeSet? = null
 ) : View(context, attributeSet) {
 
-	private val jescher = Jescher(this)
+	private val jescher = Jescher(
+			this,
+			findCurrentMovable = this::findMovableAtPoint
+	)
 	private val drawRect = Rect()
 	private val dimensionFactorRange = MAX_DIMENSION_FACTOR - MIN_DIMENSION_FACTOR
 	private val dimensionFactorRandomizer = Random()
@@ -48,6 +52,8 @@ class CustomView @JvmOverloads constructor(
 			shapes = listOf(Rectangle(Color.DKGRAY, 100F, 100F, 50F, 50F))
 		}
 	}
+
+	private fun findMovableAtPoint(point: PointF) = shapes.find { it.contains(point.x, point.y) }
 
 	override fun onDraw(canvas: Canvas) {
 		super.onDraw(canvas)
--- lib/src/main/kotlin/com/vinaysshenoy/jescher/Jescher.kt
+++ lib/src/main/kotlin/com/vinaysshenoy/jescher/Jescher.kt
@@ -3,15 +3,17 @@ package com.vinaysshenoy.jescher
 import android.graphics.Canvas
 import android.graphics.Matrix
 import android.graphics.PointF
+import android.graphics.RectF
+import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 
 class Jescher @JvmOverloads constructor(
 	private val view: View,
-	private val currentMovable: (PointF) -> Movable? = { _ -> null },
-	private val currentScalable: (PointF) -> Scalable? = { _ -> null },
-	// TODO: Register actions for onScale and onMove
-	private val interceptTouch: View.OnTouchListener = OnTouchListener { _, _ -> false }
+	private val findCurrentMovable: (PointF) -> Movable? = { _ -> null },
+	private val findCurrentScalable: (PointF) -> Scalable? = { _ -> null },
+		// TODO: Register actions for onScale,onMove,on
+	private val forwardTouchEvents: View.OnTouchListener = OnTouchListener { _, _ -> false }
 ) {
 
 	/**
@@ -28,6 +30,30 @@ class Jescher @JvmOverloads constructor(
 	 * */
 	private val tempMatrix = Matrix()
 
+	private val curTouchPoint = PointF()
+	private val prevTouchPoint = PointF()
+
+	/**
+	 * Used to hold the view area which is the target of transformation operations.
+	 *
+	 * Currently, set to the view's draw rect
+	 * */
+	private val sourceRect = RectF()
+
+	/**
+	 * Used to hold the point in the source rect to which the corresponding view touch points
+	 * map if no transformations were done
+	 **/
+	private val sourcePoint = PointF()
+
+	/**
+	 *  Used to maintain the transformed bounds to which the source rect corresponds
+	 *  after applying transformations
+	 **/
+	private val mappedRect = RectF()
+
+	private var currentMovable: Movable? = null
+
 	/**
 	 * Matrix that is provided to external callers
 	 *
@@ -40,8 +66,89 @@ class Jescher @JvmOverloads constructor(
 			return field
 		}
 
+	init {
+		view.addOnLayoutChangeListener { _, left, top, right, bottom, _, _, _, _ ->
+			sourceRect.set(
+					left.toFloat(),
+					top.toFloat(),
+					right.toFloat(),
+					bottom.toFloat()
+			)
+		}
+		view.setOnTouchListener { _, event -> handleSingleFingerTouch(event) }
+	}
+
+	private fun handleSingleFingerTouch(event: MotionEvent): Boolean {
+		curTouchPoint.set(event.x, event.y)
+
+		when (event.actionMasked) {
+			MotionEvent.ACTION_DOWN -> onSinglePointerDown()
+			MotionEvent.ACTION_CANCEL -> onSinglePointerCancel()
+			MotionEvent.ACTION_UP -> onSinglePointerUp()
+			MotionEvent.ACTION_MOVE -> onSinglePointerMove()
+		}
+
+		forwardTouchEvents.onTouch(view, event)
+		view.invalidate()
+		return true
+	}
+
+	private fun onSinglePointerDown() {
+		mapTransformationsOnSource()
+		mapPointFromTransformedToSource(curTouchPoint.x, curTouchPoint.y, sourcePoint)
+		currentMovable = findCurrentMovable(sourcePoint)
+		prevTouchPoint.set(curTouchPoint)
+	}
+
+	private fun onSinglePointerCancel() {
+		currentMovable?.let {
+			// TODO: Call appropriate callback function
+			currentMovable = null
+		}
+	}
+
+	private fun onSinglePointerUp() {
+		currentMovable?.let {
+			// TODO: Call appropriate callback function
+			currentMovable = null
+		}
+	}
+
+	private fun onSinglePointerMove() {
+		val deltaX = curTouchPoint.x - prevTouchPoint.x
+		val deltaY = curTouchPoint.y - prevTouchPoint.y
+
+		currentMovable?.moveBy(deltaX, deltaY)
+		if (currentMovable == null) {
+			transformMatrix.preTranslate(deltaX, deltaY)
+		}
+		prevTouchPoint.set(curTouchPoint)
+	}
+
+	private fun mapPointFromTransformedToSource(
+		pX: Float,
+		pY: Float,
+		holder: PointF
+	) {
+		holder.x = sourceRect.left + sourceRect.width() * ((pX - mappedRect.left) / mappedRect.width())
+		holder.y = sourceRect.top + sourceRect.height() * ((pY - mappedRect.top) / mappedRect.height())
+	}
+
+	private fun mapTransformationsOnSource() {
+		transformMatrix.mapRect(mappedRect, sourceRect)
+	}
+
 	fun apply(canvas: Canvas) {
 		canvas.concat(transformMatrix)
+	}
+
+	fun convertViewTouchPointToActual(
+		pX: Float,
+		pY: Float
+	): PointF {
+		mapTransformationsOnSource()
+		mapPointFromTransformedToSource(pX, pY, sourcePoint)
+		return sourcePoint
 	}
 
 }
